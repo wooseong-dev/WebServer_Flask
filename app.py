ab010304f28d87a6ac8c7ed536b037b9682425ad
wooseong-dev <ww0547@naver.com>
2022-10-12 10:28:11 +0900

    2022-10-12

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
-
+client=MongoClient("mongodb://localhost", 27017)
+db=client.wooseongweb
 
 @app.route("/")
 def index():
-	return 'hello world'
+	return render_template('index.html')
+
+@app.route("/write", methods=["GET", "POST"])
+def write():
+    if request.method=="POST":
+        name = request.form.get("name")
+        title = request.form.get("title")
+        contents = request.form.get("contents")
+        
+
+        inf ={
+            "name":name,
+            "title":title,
+            "contents":contents
+        }
+        db.boaard.insert_one(inf)
+        
+        print(name, title, contents)
+        
+        return ""
+    else:
+        return render_template("write.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
